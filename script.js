a54e375fa4efc4a94b4aff3a99593168983282b7
JacobMJones <theretherehuman@gmail.com>
2018-04-27 16:32:20 -0400

    Bars are added with jQuery

--- script.js
+++ script.js
@@ -12,20 +12,25 @@ function drawBarChart(data, options, element) {
 	for (var i = 0; i < data.length; i++) {
 		
 		var divName = "barDiv" + i;
+		
+		
+		$('#bar-graph').append("<div class='divName'>divName</div>").css({"background-color": "yellow", "height": "200", "width": "100"}).text(divName);
+		
+		
+		/*
+		
 		var div = document.createElement(divName);
-		div.style.width = "100px";
-		div.style.height = "100px";
-		div.style.background = "red";
-	//	div.style.color = "white";
-	//	div.innerHTML = "Hello";
-
+		div.style.width = "200px";
+		div.style.height = "500px";
+		div.style.background = "blue";
 		document.getElementById("bar-graph").appendChild(div);
+		*/
 	}
+
 }
 
 function drawGraph() {
 	drawBarChart(barArray, "none", "bar-graph");
-
 }
 
 function addBar() {
