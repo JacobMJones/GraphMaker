9125bf9d85cca3d96f3b4018717ddb30b5fec823
JacobMJones <theretherehuman@gmail.com>
2018-04-27 15:05:51 -0400

    Adds divs to interface

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-$(document).ready(function () {
 
-});
-
+var bar = {
+	height: 0,
+	width: 0,
+	color: "",
+	name: "",
+	value: "",
+}
+var graphVariables = [];
+var graphValues = [];
 
 function drawBarChart(data, options, element){
 	
+	
 }
+
+function addBar(){
+	
+}
+
+$(document).ready(function () {
+
+});
