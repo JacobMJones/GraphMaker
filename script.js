b75989363dcf66d1dbe86296362f1067297ed7d7
JacobMJones <theretherehuman@gmail.com>
2018-04-28 07:35:21 -0400

    Bars are spaced by percentage

--- script.js
+++ script.js
@@ -9,34 +9,21 @@ var barArray = [];
 
 function drawBarChart(data, options, element) {
 
+
+
 	var myHtml = "";
- 
-style="color:blue;"
-	
-	var myHtml = "";
-	
+var distance = 1;
 	for (var i = 0; i < data.length; i++) {
 	
 		var divName = "barDiv" + i;
-		var distance = i * 10;
+		var distance = i + 3 +"%";
 		var col = randomColor();
 		myHtml += "<div class='bar' style=" + "margin-left:" + distance + ";" + "background-color:" + col  + ";" + "height:200px" + ">" + divName + "</div>";
 		
-		$( "#bar-graph" ).html(myHtml);
-		//$('#bar-graph').append("<div class='bar'>divName</div><br>").css({"background-color": col, "height": "200", "width": "100"}).text(divName);
-		
-	//$('#bar-graph').append("<div class='bar'></div>").append("<p>dd</p>").css({"background-color": "col", "float": "left", "height": "200", "width": "50%"});
+	
 
-		/*
-		
-		var div = document.createElement(divName);
-		div.style.width = "200px";
-		div.style.height = "500px";
-		div.style.background = "blue";
-		document.getElementById("bar-graph").appendChild(div);
-		*/
 	}
-
+	$( "#bar-graph" ).html(myHtml);
 }
 function randomColor(){
 	var c = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
