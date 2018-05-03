1d9701609f2e7eef7ec9da56e33cf52554f9b615
JacobMJones <theretherehuman@gmail.com>
2018-05-03 18:13:12 -0400

    drawBarChart is now comprised of functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var barArray = [];
 var sections = [];
 
@@ -17,130 +15,18 @@ var graphValuesDivWidth;
 var barGraphDiv;
 var graphLabelDiv;
 var graphValuesDiv;
-
+var highestValue;
 
 function drawBarChart(data, options, element) {
-
-	createDivs(data, options, element);
-	graph(data, options, element);
-    labels(data,options,element);
-	/*
-	//create three divs (graph, values, labels)
-	
-
-	//create html strings to populate divs with
-
-	var graphHtml = "";
-	var valuesHtml = "";
-	var labelsHtml = "";
-
-	var barGraphWidth = $(barGraphDiv).width();
-	var lineWidth = barGraphWidth + barGraphWidth / 10;
-	var barGraphHeight = $(barGraphDiv).height();
-
-	//Figures out the highest value and proportions according to graph size
-	var valuesArray = [];
-	for (u = 0; u < data.length; u++) {
-		valuesArray.push(data[u].value);
-	}
-	var highestValue = Math.max(...valuesArray);
-	var barHeightModifier = (barGraphDivHeight * .9) / highestValue;
-	console.log(barHeightModifier);
-
-
-
-
-	var toptopValue = Math.ceil((highestValue * 1.1) / 10) * 10;
-
-	var midValue = toptopValue / 2
-
-	var bottomValue = midValue / 2;
-
-	var topValue = midValue + bottomValue;
-
-
-
-	//setup sections and info
-	for (var i = 0; i < data.length; i++) {
-
-		section = {
-			divName: "barDiv" + i,
-			variableName: data[i].name,
-			variableValue: data[i].value,
-			sectionHeight: data[i].value + (barGraphHeight / 4),
-			color: randomColor(),
-			barHeight: data[i].value * barHeightModifier,
-			sectionWidth: (90 / data.length) - (5 / data.length) + "%",
-			distance: (100 / data.length) * i + "%"
-		}
-		sections.push(section);
-	}
-
-
-	var labelDist = barGraphDivWidth / sections.length;
-
-	for (var s = 0; s < sections.length; s++) {
-
-		// section and bar
-		graphHtml += "<div class='bar' style=" + "height:" + sections[s].barHeight + ";" + "background-color:" + sections[s].color + ";" + "bottom:" + ((divContainerHeight * .02)) + ";" + "width:" + sections[s].sectionWidth + ";" + "margin-left:" + sections[s].distance + ";" + "text-align:" + "center" + ";" + "></div>";
-
-		//Name
-
-		labelsHtml += "<div class='barName' style=" + "bottom:" + ((divContainerHeight * .1) / 1.3) + ";" + "margin-left:" + ((labelDist * s) + (labelDist / data.length)) + ";" + ">" + sections[s].variableName + "</div>";
-
-		//Value (this is in graph Html as it appears in graph)
-		graphHtml += "<div class='barValue' style=" + "bottom:" + sections[s].barHeight + ";" + "margin-left:" + sections[s].distance + ";" + ">" + sections[s].variableValue + "</div>";
-
-	}
-
-	//value lines 
-	var midValLine = barGraphHeight / 2;
-
-	var bottomValLine = midValLine / 2;
-
-	var topValLine = midValLine + bottomValLine;
-
-
-
-	graphHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + midValLine + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
-
-	graphHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + bottomValLine + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
-
-
-	graphHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + topValLine + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
-
-	graphHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + 0 + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "padding:" + "2" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
-
-	graphHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
-
-	//values
-
-
-	//Top Top
-	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "top:" + 0 + ";" + ">" + toptopValue + "</p>";
-
-
-
-	//2nd Top
-	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + (topValLine + (divContainerHeight * .075)) + ";" + ">" + topValue + "</p>";
-
-	//mid
-	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + (midValLine + (divContainerHeight * .075)) + ";" + ">" + midValue + "</p>";
-
-	//bottom
-	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + (bottomValLine + (divContainerHeight * .075)) + ";" + ">" + bottomValue + "</p>";
-
-	//zero
-	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + divContainerHeight * .075 + ";" + ">" + 0 + "</p>";
-
-	//append everything
-	$(barGraphDiv).append(graphHtml);
-	$(graphLabelDiv).append(labelsHtml);
-	$(graphValuesDiv).append(valuesHtml);
-*/
+	createDivs(element);
+	graph(data, options);
+	labels(data, options);
+	lines();
+	innerValues(data, options, element);
+	outerValues(data, options, element);
 }
 
-function createDivs(data, options, element) {
+function createDivs(element) {
 
 	divContainer = $(element);
 	divContainerHeight = $(element).height();
@@ -176,7 +62,7 @@ function graph(data, options, element) {
 	for (u = 0; u < data.length; u++) {
 		valuesArray.push(data[u].value);
 	}
-	var highestValue = Math.max(...valuesArray);
+	highestValue = Math.max(...valuesArray);
 	var barHeightModifier = (barGraphDivHeight * .9) / highestValue;
 
 	//prepares section object which will be used in html string
@@ -190,7 +76,8 @@ function graph(data, options, element) {
 			color: randomColor(),
 			barHeight: data[i].value * barHeightModifier,
 			sectionWidth: (90 / data.length) - (5 / data.length) + "%",
-			distance: (100 / data.length) * i + "%"
+			distance: (100 / data.length) * i + "%",
+			distanceInt: ((100 / data.length) * i)
 		}
 		sections.push(section);
 	}
@@ -203,22 +90,69 @@ function graph(data, options, element) {
 	}
 
 	$(barGraphDiv).append(graphHtml);
-
-
 }
 
 function labels(data, options, element) {
-	
+
 	var labelsHtml = "";
 	var labelDist = graphLabelDivWidth / sections.length;
-	
+
 	for (var s = 0; s < sections.length; s++) {
-		
-	labelsHtml += "<div class='barName' style=" + "bottom:" + graphLabelDivHeight + ";" + "margin-left:" + ((labelDist * s) + (labelDist / data.length)) + ";" + ">" + sections[s].variableName + "</div>";
+
+		labelsHtml += "<div class='barName' style=" + "bottom:" + graphLabelDivHeight + ";" + "margin-left:" + ((labelDist * s) + (labelDist / data.length)) + ";" + ">" + sections[s].variableName + "</div>";
 	}
-	
+
 	$(graphLabelDiv).append(labelsHtml);
 }
+
+function lines() {
+	
+	var lineHtml = "";
+	var midValLine = barGraphDivHeight / 2;
+	
+
+	//bottom line
+	lineHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + 0 + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
+
+	//middle line
+	lineHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "bottom:" + midValLine + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
+
+	//top line
+	lineHtml += "<hr class='valueLine' style=" + "background:" + "grey" + ";" + "top:" + 0 + ";" + "size:" + "1px" + ";" + "height:" + "1" + ";" + "margin-left:" + "-15" + ";" + "width:" + (barGraphDivWidth + 15) + ";" + ">";
+
+	$(barGraphDiv).append(lineHtml);
+}
+
+function innerValues(data, options, element) {
+
+	var valuesHtml = "";
+	var labelDist = graphLabelDivWidth / sections.length;
+	for (var s = 0; s < sections.length; s++) {
+		valuesHtml += "<div class='barValue' style=" + "bottom:" + (sections[s].barHeight + 10) + ";" + "margin-left:" + ((labelDist * s) + (labelDist / data.length)) + ";" + ">" + sections[s].variableValue + "</div>";
+	}
+	$(barGraphDiv).append(valuesHtml);
+}
+
+function outerValues(data, options, element) {
+
+	//Values 
+	var toptopValue = Math.ceil((highestValue * 1.1) / 10) * 10;
+
+	var midValue = toptopValue / 2
+	var midValPosition = divContainerHeight /2;
+	
+	var valuesHtml = "";
+	
+	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "top:" + 0 + ";" + ">" + toptopValue + "</p>";
+	
+	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + midValPosition + ";" + ">" + midValue + "</p>";
+	
+	valuesHtml += "<p style=" + "position:" + "absolute" + ";" + "bottom:" + (divContainerHeight * .05) + ";" + ">" + 0 + "</p>";
+	
+	$(graphValuesDiv).append(valuesHtml);
+}
+
+
 
 function randomColor() {
 	var c = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
